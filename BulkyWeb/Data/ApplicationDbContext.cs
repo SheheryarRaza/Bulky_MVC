204dfed3fe4cd1965b8b2f635744a52f7e7786d0
Sheheryar <m.sheheryarraza@gmail.com>
2025-02-20 01:09:29 +0500

    Added DbSet and some Basic Configurations

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
