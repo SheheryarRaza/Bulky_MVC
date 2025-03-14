4e898df9bd516a12c2d2132f4fb7d73674bdca8f
Sheheryar <m.sheheryarraza@gmail.com>
2025-03-14 17:55:48 +0500

    Added Delete functionality in Product.js

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     loadDataTable();
 });
-
+var dataTable;
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
@@ -16,7 +16,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                         <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit </a>
-                        <a href="" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete </a>
+                        <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete </a>
 
                         </div>
                         `
@@ -24,5 +24,32 @@ function loadDataTable() {
                 "width": "25%"
             }
         ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
