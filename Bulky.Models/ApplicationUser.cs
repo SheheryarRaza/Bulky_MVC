39e5719fec2ec2bafc540387b0024840ef96f1c1
Sheheryar <m.sheheryarraza@gmail.com>
2025-04-16 16:00:23 +0500

    Added Order View Model

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -20,7 +20,7 @@ namespace Bulky.Models
         public string? State { get; set; }
         public string? PostalCode { get; set; }
 
-        public int CompanyId { get; set; }
+        public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
         public Company Company { get; set; }
